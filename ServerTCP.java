902bbab690507d0ed02e92bf1161f652578bff72
Katelyn Peterson <katelynpeterson@KatelynP.localdomain>
2018-03-07 20:43:39 -0700

    updated file

--- ServerTCP.java
+++ ServerTCP.java
@@ -1,8 +1,8 @@
 	import java.io.BufferedReader;
-import java.io.DataOutputStream;
+	import java.io.DataOutputStream;
 	import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+	import java.io.InputStreamReader;
+	import java.io.OutputStream;
 	import java.net.ServerSocket;
 	import java.net.Socket;
 	import java.net.InetAddress;
@@ -11,7 +11,7 @@ public class ServerTCP {
 	
 	private ServerSocket server;
 	
-	public static void main(String args[]) throws Exception{
+	public static void main(String[] args) throws IOException{
 		
         ServerTCP app = new ServerTCP(args[0]);
         
@@ -41,25 +41,43 @@ public class ServerTCP {
 	ss.close();*/
 		
 	}
-    public ServerTCP(String ipAddress) throws Exception {
+    public ServerTCP(String ipAddress) throws IOException {
         if (ipAddress != null && !ipAddress.isEmpty()) 
           this.server = new ServerSocket(0, 1, InetAddress.getByName(ipAddress));
         else 
           this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
     }
 	
-    private void listen() throws Exception {
+    protected void listen() throws IOException {
+	Socket client = null;
         String data = null;
-        Socket client = this.server.accept();
+
+	int numPlayers = 0;	
+
+	while(numPlayers <3){
+		try{
+		client = this.server.accept();
+		}catch(IOException e){
+		System.out.println("IO Error " + e);
+		}
+
+		new EchoThread(client).start();
+		numPlayers +=1;
+	
+		
+        //Socket client = this.server.accept();
         String clientAddress = client.getInetAddress().getHostAddress();
-        System.out.println("\r\nNew connection from " + clientAddress);
+	//String peerName = client.getpeername();
+	// int portName = client.getInetAddress().getPort();
+        System.out.println("\r\nNew connection from player" + numPlayers + " (" + clientAddress  + ")");
+      // System.out.println("port name " +portName); 
+       }
+//	 BufferedReader in = new BufferedReader(
+  //              new InputStreamReader(client.getInputStream()));  
         
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(client.getInputStream()));  
-        
-        while ( (data = in.readLine()) != null ) {
-            System.out.println("\r\nMessage from " + clientAddress + ": " + data);
-        }
+       // while ( (data = in.readLine()) != null ) {
+         //   System.out.println("\r\nMessage from " + clientAddress + ": " + data);
+       // }
     }
     
     public InetAddress getSocketAddress() {
@@ -69,4 +87,24 @@ public class ServerTCP {
     public int getPort() {
         return this.server.getLocalPort();
     }
+}
+
+class Player implements Runnable{
+	private String threadName;
+	private	Thread t;
+
+	Player(String name){
+	threadName = name;
+}
+
+public void run(){
+	System.out.println(threadName + " is running");
+}
+
+public void start(){
+	if(t==null){
+		t = new Thread (this, threadName);
+		t.start();
+		}
+	}
 }
